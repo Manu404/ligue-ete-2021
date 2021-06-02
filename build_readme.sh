c71ef997394279238540645178ee2c50c34548f2
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2021-06-02 19:37:06 +0200

    clean root

--- build_readme.sh
+++ build_readme.sh
@@ -2,7 +2,7 @@
 
 
 cat /dev/null > readme.md
-cat toc.md >> readme.md
-cat organisation.md >> readme.md
-cat reglement.md >> readme.md
-cat points.md >> readme.md
+cat readme.content/toc.md >> readme.md
+cat readme.content/organisation.md >> readme.md
+cat readme.content/reglement.md >> readme.md
+cat readme.content/points.md >> readme.md
