717f0d2fa9cb80038c41cccd674a360344bad19c
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2021-06-02 19:07:41 +0200

    add mcse template (need cleanup files)

---
+++ babioles/magic-new-ligue-babiole.mse-style/build.bat
@@ -0,0 +1,7 @@
+cd ..
+
+cd ..
+
+mse --create-installer magic-new-ligue-babiole.mse-style magic-new.mse-style magic.mse-game magic-blends.mse-include magic-mana-small.mse-symbol-font magic-mana-large.mse-symbol-font magic-watermarks.mse-include magic-identity-new.mse-include
+
+ren "magic-new-ligue-babiole.mse-installer" "Ligue - Babiole.mse-installer"
