4396228734dbe8e42c1a0f3d7f370af0a89bec54
istace.emmanuel@hotmail.com <istace.emmanuel@hotmail.com>
2021-06-04 10:39:25 +0200

    clean build script

--- build_readme.sh
+++ build_readme.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-# build resource folder
-rm -rf resources
-mkdir -p resources/babioles
-cp readme.content/resources/*.png ./resources
-cp readme.content/resources/babioles/*.png ./resources/babioles
+# append_section "header" "readme" => echo "" >> readme.md && cat readme.content/header.md >> readme.md
+append_section(){
+	echo "" >> $2.md && cat $2.content/$1.md >> $2.md
+}
 
-# build readme
-cat /dev/null > readme.md
-echo "" >> readme.md && cat readme.content/header.md >> readme.md
-echo "" >> readme.md && cat readme.content/toc.md >> readme.md
-echo "" >> readme.md && cat readme.content/doodle.md >> readme.md
-echo "" >> readme.md && cat readme.content/organisation.md >> readme.md
-echo "" >> readme.md && cat readme.content/reglement.md >> readme.md
-echo "" >> readme.md && cat readme.content/points.md >> readme.md
+clear_file() {
+	cat /dev/null > $1.md
+}
+
+build_resource() {
+	rm -rf resources
+	mkdir -p resources/babioles
+	cp readme.content/resources/*.png ./resources
+	cp readme.content/resources/babioles/*.png ./resources/babioles
+}
+
+build_resource
+
+clear_file "readme"
+append_section "header" "readme"
+append_section "toc" "readme"
+append_section "doodle" "readme"
+append_section "organisation" "readme"
+append_section "reglement" "readme"
+append_section "points" "readme"
